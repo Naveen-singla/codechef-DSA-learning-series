f79594c76aa213a6ed851c8507bd8723eb7266a2
Naveen-singla <Naveensingla2001@gmail.com>
2022-01-06 21:48:38 +0530

    is both or not solution added

---
+++ easy/is_both_or_not.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
+
+int n;
+cin >> n;
+if(n%5==0 && n%11==0) cout << "BOTH\n";
+else if(n%5==0 || n%11==0) cout << "ONE\n";
+else cout << "NONE\n";
+return 0;
+}
